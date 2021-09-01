95f5e594b3f2648ab58da7525e964422b60a9a7f
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-08-31 17:51:59 -0700

    adds options to menu method in CLI class

--- lib/cli.rb
+++ lib/cli.rb
@@ -7,6 +7,11 @@ class CLI
 
     def menu
         puts 'please choose an option'
+        puts '1. all astronauts'
+        puts '2. all spacecraft'
+        puts '3. astronauts by country'
+        puts '4. spacecraft by country'
+        
         @input = gets.strip
     end
 
