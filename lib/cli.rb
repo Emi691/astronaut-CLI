235617ec4f3156ac3f9a78ac7671d515a50e32b5
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-08-31 20:55:37 -0700

    defines directory method in cli Class

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,4 @@
 class CLI 
-
     def hello
         puts 'Welcome to Astro CLI'
         API.get_astros
@@ -12,6 +11,21 @@ class CLI
         puts '2. all spacecraft'
         puts '3. current location of the ISS'
         input = gets.strip
+        self.directory(input)
     end
 
+    def directory(input)
+        if input == 1
+            self.astros
+        elsif input == 2
+            self.craft
+        elsif input == 3
+            self.iss_loc
+        elsif input == exit
+            self.exit
+        else
+            puts 'Option not found'
+            self.menu
+        end
+    end
 end
