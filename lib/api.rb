283721d675062fa3d301843bf31786c2dc9ce0d8
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-08-31 21:07:28 -0700

    defines get_iss class method for API class

--- lib/api.rb
+++ lib/api.rb
@@ -1,12 +1,16 @@
 class API
-
-    @@url = 'http://api.open-notify.org/astros.json'
-
     def self.get_astros
-        response = HTTParty.get(@@url)
+        url = 'http://api.open-notify.org/astros.json'
+        response = HTTParty.get(url)
         response["people"].each do |astro|
             Astronaut.new(astro)
         end
     end
 
+    def self.get_iss
+        url = 'http://api.open-notify.org/iss-now.json'
+        response = HTTParty.get(url)
+        loc = response["iss_position"]
+        ISS.new(loc)
+    end
 end
