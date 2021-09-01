4f8cdec49b502ec5119c8c9bd352fd6962a1529c
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-08-31 17:41:34 -0700

    defines menu method for CLI class

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,6 +2,12 @@ class CLI
 
     def hello
         puts 'Welcome to Astro CLI'
+        self.menu
+    end
+
+    def menu
+        puts 'please choose an option'
+        @input = gets.strip
     end
 
 end
