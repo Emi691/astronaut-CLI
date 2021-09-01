93805bd74ceb3945ea903b968f97cb640f921b13
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-08-31 21:12:48 -0700

    defines all and newest class methods for ISS class

--- lib/iss.rb
+++ lib/iss.rb
@@ -5,6 +5,13 @@ class ISS
 
     def initialize(hash)
         hash.each {|key, value| self.send("#{key}=", value)}
-        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.newest
+        self.all.last
     end
 end
