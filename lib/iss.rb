2a4df878ab7f87868562288756a9263ee518eb84
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-08-31 21:09:36 -0700

    defines intialize method, all class variable, and attr_accessor for ISS class

--- lib/iss.rb
+++ lib/iss.rb
@@ -1,3 +1,10 @@
 class ISS
-    
+    attr_accessor :longitude, :latitude
+
+    @@all = []
+
+    def initialize(hash)
+        hash.each {|key, value| self.send("#{key}=", value)}
+        @@all << self
+    end
 end
